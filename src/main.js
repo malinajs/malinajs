5c53b895434b955f95af707a1ca622bad47e894e
Oleg Nechaev <lega911@gmail.com>
2020-06-26 23:30:23 +0200

    options for compiler

--- src/main.js
+++ src/main.js
@@ -2,7 +2,32 @@
 import fs from 'fs';
 import { compile } from './compiler';
 
-let inputFile = process.argv[2] || './example/example.html';
+let opt, inputFile, outputFile = './bin/output.js', widgetName = 'widget';
+process.argv.slice(2).forEach(s => {
+    if(s[0] == '-') {
+        opt = s;
+        return;
+    }
+    if(opt == '-o') {
+        outputFile = s;
+        opt = null;
+        return;
+    }
+    if(opt == '-n') {
+        widgetName = s;
+        opt = null;
+        return;
+    }
+    if(opt == '-i') {
+        inputFile = s;
+        opt = null;
+        return;
+    }
+    if(inputFile) throw 'Wrong options';
+    inputFile = s;
+});
+
+if(!inputFile) throw 'No input file';
 const src = fs.readFileSync(inputFile, {encoding:'utf8', flag:'r'}); 
-const result = compile(src, {name: 'widget'});
-fs.writeFileSync('./bin/output.js', result, {encoding:'utf8', flag:'w'}); 
+const result = compile(src, {name: widgetName});
+fs.writeFileSync(outputFile, result, {encoding:'utf8', flag:'w'}); 
