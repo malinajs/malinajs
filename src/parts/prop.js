33a82d284eba8a87ba0702a6e41f0e5bbefc1c72
Oleg Nechaev <lega911@gmail.com>
2020-07-18 18:08:51 +0200

    event forwarding for elements

--- src/parts/prop.js
+++ src/parts/prop.js
@@ -54,6 +54,15 @@ export function bindProp(prop, makeEl, node) {
         if(prop.value) {
             exp = getExpression();
         } else {
+            if(!opts.length) {
+                // forwarding
+                return {bind: `
+                    $cd.ev(${makeEl()}, "${event}", ($event) => {
+                        const fn = $option.events && $option.events.${event};
+                        if(fn) fn($event);
+                    });\n`
+                };
+            }
             handler = opts.pop();
         };
         assert(event, prop.content);
