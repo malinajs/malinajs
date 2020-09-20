df7a48d710ff44b56cf370c8d1eedc6dd02254a7
Oleg Nechaev <lega911@gmail.com>
2020-09-20 23:27:40 +0200

    0.5.22

--- src/compiler.js
+++ src/compiler.js
@@ -5,7 +5,7 @@ import { transformJS } from './code';
 import { buildRuntime } from './builder';
 import { processCSS } from './css/index';
 
-export const version = '0.5.21';
+export const version = '0.5.22';
 
 export function compile(src, config = {}) {
     if(!config.name) config.name = 'widget';
