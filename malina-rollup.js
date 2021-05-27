0733a2907d7220aaec6c7eeb0362adadb4ab22c5
huangteng02 <huangteng02@meituan.com>
2021-05-27 20:02:12 +0800

    feat: sematic optimization

--- malina-rollup.js
+++ malina-rollup.js
@@ -3,21 +3,21 @@ const malina = require('malinajs');
 
 module.exports = malinaRollup;
 
-function malinaRollup(option = {}) {
-    if(option.displayVersion !== false) console.log('! Malina.js', malina.version);
-    if(!option.extension) option.extension = ['html', 'ma', 'xht'];
+function malinaRollup(options = {}) {
+    if(options.displayVersion !== false) console.log('! Malina.js', malina.version);
+    if(!options.extension) options.extension = ['html', 'ma', 'xht'];
     let content_cache = {};
 
     return {
         name: 'malina',
         async transform(code, id) {
-            if(!option.extension.some(ext => id.endsWith('.' + ext))) return null;
+            if(!options.extension.some(ext => id.endsWith('.' + ext))) return null;
             let result;
 
             let opts = Object.assign({
                 path: id,
                 name: id.match(/([^/\\]+)\.\w+$/)[1]
-            }, option);
+            }, options);
             try {
                 let ctx = await malina.compile(code, opts);
                 result = ctx.result;
