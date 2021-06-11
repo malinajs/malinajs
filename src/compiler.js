1fe8cb18e593141cd776defc9fb0f56ac907a849
Oleg Nechaev <lega911@gmail.com>
2021-06-11 23:47:21 +0200

    0.6.23

--- src/compiler.js
+++ src/compiler.js
@@ -17,7 +17,7 @@ import { attachSlot } from './parts/slot.js'
 import { makeFragment, attachFragment } from './parts/fragment.js'
 
 
-export const version = '0.6.22';
+export const version = '0.6.23';
 
 
 export async function compile(source, config = {}) {
