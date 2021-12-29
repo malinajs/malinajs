7063ca8514cfbaf765b179a0a487510623dd9a49
Oleg Nechaev <lega911@gmail.com>
2021-12-29 22:51:56 +0100

    retire compile.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 
 export default [{
-	input: './src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'cjs',
-		file: './compile.js',
-		globals: ['acorn', 'astring', 'css-tree']
-    },
-	external: ['fs', 'acorn', 'astring', 'css-tree'],
-	plugins: [commonjs()]
-}, {
 	input: './src/compiler.js',
 	output: {
 		sourcemap: true,
