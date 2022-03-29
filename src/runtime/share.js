0fdb76d010e3d3ce02dde2194350703a2d446b69
Artemiy Schukin <shuckinart@yandex.ru>
2022-03-29 15:51:39 +0300

    fix: Reduce `runtime/share.js

--- src/runtime/share.js
+++ src/runtime/share.js
@@ -4,24 +4,9 @@ const $onDestroy = (fn) => fn && current_destroyList.push(fn);
 
 const share = {
   $onDestroy,
-  get current_destroyList() {
-    return current_destroyList;
-  },
-  set current_destroyList(value) {
-    current_destroyList = value;
-  },
-  get current_cd() {
-    return current_cd;
-  },
-  set current_cd(value) {
-    current_cd = value;
-  },
-  get destroyResults() {
-    return destroyResults;
-  },
-  set destroyResults(value) {
-    destroyResults = value;
-  }
+  current_destroyList,
+  current_cd,
+  destroyResults
 };
 
 export { share, $onDestroy };
