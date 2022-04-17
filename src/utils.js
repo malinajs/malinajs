73329d951bafec7b018a49e84c8b18b9d63c2bd7
Oleg Nechaev <lega911@gmail.com>
2022-04-17 16:25:13 +0200

    fix check identificator

--- src/utils.js
+++ src/utils.js
@@ -77,17 +77,8 @@ export function detectExpressionType(name) {
   function checkIdentificator(body) {
     if(body.length != 1) return;
     if(body[0].type != 'ExpressionStatement') return;
-    if(body[0].expression.type != 'Identifier') return;
-    return true;
-  }
-
-  function checkMemberIdentificator(body) {
-    if(body.length != 1) return;
-    if(body[0].type != 'ExpressionStatement') return;
     let obj = body[0].expression;
-    if(obj.type != 'MemberExpression') return;
-    if(obj.property.type != 'Identifier') return;
-    return true;
+    return obj.type == 'Identifier' || obj.type == 'MemberExpression';
   }
 
   function checkFunction(body) {
@@ -107,7 +98,6 @@ export function detectExpressionType(name) {
   }
 
   if(checkIdentificator(ast.body)) return 'identifier';
-  if(checkMemberIdentificator(ast.body)) return 'identifier';
   if(checkFunction(ast.body)) return 'function';
 
   let fn = checkFunctionCall(ast.body);
