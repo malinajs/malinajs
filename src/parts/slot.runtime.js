8a9c09418fb4001e95e5a6f895f027a19ba21c3c
Oleg Nechaev <lega911@gmail.com>
2022-04-20 23:43:55 +0200

    fix slot

--- src/parts/slot.runtime.js
+++ src/parts/slot.runtime.js
@@ -3,7 +3,7 @@ import * as share from '../runtime/share.js';
 
 export const invokeSlotBase = ($component, slotName, $context, props, placeholder) => {
   let $slot = $component.$option.slots?.[slotName || 'default'];
-  return $slot ? $slot($component, $context, props) : placeholder?.();
+  return $slot ? $slot($component, $context, props)[0] : placeholder?.();
 };
 
 export const invokeSlot = ($component, slotName, $context, propsFn, placeholder, cmp) => {
@@ -26,6 +26,7 @@ export const makeSlot = (fr, fn) => {
     let $dom = fr.cloneNode(true), prev = share.current_cd, $cd = share.current_cd = cd_new();
     cd_attach2(parentCD, $cd);
     share.$onDestroy(() => cd_detach($cd));
+    parentCD.component.apply();
     try {
       return [$dom, fn($dom, $context, callerComponent, props)];
     } finally {
