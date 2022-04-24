a417f1763618506ca6e11d4bbadc9911960963ea
Oleg Nechaev <lega911@gmail.com>
2022-04-24 22:04:58 +0200

    0.7.0-a8

--- src/compiler.js
+++ src/compiler.js
@@ -21,7 +21,7 @@ import { attachPortal } from './parts/portal.js';
 import { makeEventProp } from './event-prop.js';
 
 
-export const version = '0.7.0-a7';
+export const version = '0.7.0-a8';
 
 
 export async function compile(source, config = {}) {
