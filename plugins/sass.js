1fa358ae23ed1719d2bb41bc9b94e0f58a254eec
Oleg Nechaev <lega911@gmail.com>
2023-03-01 22:13:25 +0100

    Fix for sass/scss plugin

--- plugins/sass.js
+++ plugins/sass.js
@@ -8,17 +8,35 @@ module.exports = function sassPlugin() {
                 let type = node.attributes.filter(a => a.name == 'type' || a.name == 'lang')[0];
                 if(!type || type.value != 'sass' && type.value != 'scss') continue;
 
-                const sass = require('sass');
+                let sass;
+                try {
+                    sass = require('sass');
+                } catch (e) {
+                    if(e.code == 'MODULE_NOT_FOUND') sass = require('node-sass');
+                    else throw e;
+                }
                 node.content = await (new Promise((resolve, reject) => {
-                    sass.render({
-                        file: ctx.config.path,
-                        data: node.content,
-                        indentedSyntax: type.value == 'sass'
-                    }, function(e, result) {
-                        if(e) return reject(e);
-                        resolve(result.css.toString());
-                        type.value = 'css';
-                    });
+                    if(sass.render) {
+                        sass.render({  // node-sass
+                            file: ctx.config.path,
+                            data: node.content,
+                            indentedSyntax: type.value == 'sass'
+                        }, function(e, result) {
+                            if(e) return reject(e);
+                            resolve(result.css.toString());
+                            type.value = 'css';
+                        });
+                    } else {
+                        sass.compileStringAsync(node.content, {
+                            syntax: type.value == 'sass' ? 'indented' : 'scss',
+                            url: 'file://' + ctx.config.path
+                        }).then((r) => {
+                            resolve(r.css);
+                        }, (e) => {
+                            console.error('SCSS Error', e);
+                            reject(e);
+                        })
+                    }
                 }));
             };
         }
