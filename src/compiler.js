50a3632f3db054c287019c1bd919e533b6edf6f1
Oleg Nechaev <lega911@gmail.com>
2023-07-10 10:08:02 +0200

    0.7.3

--- src/compiler.js
+++ src/compiler.js
@@ -23,7 +23,7 @@ import { makeEventProp } from './event-prop.js';
 import { makeKeepAlive } from './parts/keep-alive.js';
 
 
-export const version = '0.7.2-a12';
+export const version = '0.7.3';
 
 
 export async function compile(source, config = {}) {
